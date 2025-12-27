eceb8837c95bcbb6d77c5d63ad7a2a9f785e571b
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2025-12-27 17:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const firstname= document.getElementByName("fname").value.trim();
-	const lastname = document.getElementByName("lname").value.trim();
+	const firstname= document.querySelector("#forml[name='fname']").value.trim();
+	const lastname = document.querySelector("#forml[name='lname']").value.trim();
 	alert(`${firstname} ${lastname}`);
 }
